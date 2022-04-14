9730a56fd7295d11651c39cd9bd175744e0648f6
s-ike <shunt_i@yahoo.co.jp>
2022-04-14 22:04:31 +0900

    add flash message

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,6 +6,13 @@
     </x-slot>
 
     <div class="py-12">
+        @if (session('message'))
+            <div class="sm:px-6 lg:px-8">
+                <div class="bg-blue-500 max-w-7xl mx-auto p-2 my-4 text-white">
+                    {{ session('message') }}
+                </div>
+            </div>
+        @endif
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
